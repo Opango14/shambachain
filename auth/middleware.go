fe9b0ed4aab7eabf504bfc1d1d3873077bb72569
keyadaniel56 <keyadaniel56@gmail.com>
2026-05-28 13:38:40 +0300

    feat : implemented the authmiddleware

--- auth/middleware.go
+++ auth/middleware.go
@@ -1,1 +1,36 @@
 package auth
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "authorization header required"})
+			return
+		}
+		part := strings.SplitN(authHeader, " ", 2)
+		if len(part) != 2 || strings.ToLower(part[0]) != "bearer" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid authorization format",
+			})
+			return
+		}
+		tokenString := strings.TrimSpace(part[1])
+
+		if tokenString == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "token missing",
+			})
+			return
+		}
+
+		ctx.Set("token", tokenString)
+		ctx.Next()
+	}
+}
